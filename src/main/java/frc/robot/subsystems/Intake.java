b383ad99e7c452e1e09f9ee465016a1f4d6f9c04
weepfrog <housmanzach@gmail.com>
2024-01-21 00:01:24 -0500

    added intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,23 @@
 package frc.robot.subsystems;
 
-public class Intake {
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
     
+    CANSparkMax m_intakeMotor;
+
+    public Intake(){
+        m_intakeMotor = new CANSparkMax(Constants.INTAKE_MOTOR_CAN_ID, CANSparkMax.MotorType.kBrushless);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    public void run(double speed) {
+        m_intakeMotor.set(-speed);
+    }
 }
