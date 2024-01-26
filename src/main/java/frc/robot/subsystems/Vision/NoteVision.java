4fd594ec0e0fe2052ef54a640bac9942d1255bd2
weepfrog <housmanzach@gmail.com>
2024-01-25 20:07:02 -0500

    added finding note pose

--- src/main/java/frc/robot/subsystems/Vision/NoteVision.java
+++ src/main/java/frc/robot/subsystems/Vision/NoteVision.java
@@ -6,7 +6,8 @@
 
     import java.io.IOException;
     import java.util.ArrayList;
-    import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
     import org.photonvision.EstimatedRobotPose;
     import org.photonvision.PhotonCamera;
@@ -54,107 +55,49 @@ import frc.robot.Robot;
         static final double SHED_TAG_NODE_ZOFFSET = 0.31;
         static final double SHED_TAG_SUBSTATION_YOFFSET = 1.19;
 
-        private static final String CAMERA_NAME = "ApriltagCamera";
-        private final PhotonCamera m_aprilTagCamera = new PhotonCamera(CAMERA_NAME);
-        private AprilTagFieldLayout m_aprilTagFieldLayout;
+        private static final String CAMERA_NAME = "NoteCamera";
+        private final PhotonCamera m_noteCamera = new PhotonCamera(CAMERA_NAME);
 
         // Forward B&W camera for Apriltags
         // relative position of the camera on the robot ot the robot center
-        private final Transform3d m_robotToAprilTagCam = new Transform3d(
+        private final Transform3d m_robotToNoteCam = new Transform3d(
                 new Translation3d(Units.inchesToMeters(3.5), -0.136, Units.inchesToMeters(24.75)),
                 new Rotation3d(0.0, 0.0, 0.0));
 
-        private final PhotonPoseEstimator m_photonPoseEstimator;
 
         // Simulation support
         private VisionSystemSim m_visionSim;
 
         public NoteVision() {
-            try {
-                m_aprilTagFieldLayout = AprilTagFieldLayout
-                            .loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
-            } catch (IOException e) {
-                System.out.println("Unable to load AprilTag layout " + e.getMessage());
-                m_aprilTagFieldLayout = null;
-            }
-
-            if (Constants.SIMULATION_SUPPORT) {
-                // initialize a simulated camera. Must be done after creating the tag layout
-                initializeSimulation();
-            }
-
-            if (USE_MULTITAG) {
-                m_photonPoseEstimator = new PhotonPoseEstimator(m_aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-                        m_aprilTagCamera, m_robotToAprilTagCam);
-                m_photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.CLOSEST_TO_REFERENCE_POSE);
-            } else {
-                m_photonPoseEstimator = new PhotonPoseEstimator(m_aprilTagFieldLayout,
-                        PoseStrategy.CLOSEST_TO_REFERENCE_POSE, m_aprilTagCamera, m_robotToAprilTagCam);
-            }
+            
 
             // set the driver mode to false
-            m_aprilTagCamera.setDriverMode(false);
+            m_noteCamera.setDriverMode(false);
         }
 
-        public void updateSimulation(Pose2d pose) {
-            m_visionSim.update(pose);
+        // public void updateSimulation(Pose2d pose) {
+        //     m_visionSim.update(pose);
+        // }
+        public List<Pose2d> getNotes(){
+            if(!m_noteCamera.isConnected()){
+                return null;
+            }
+
+            var results = m_noteCamera.getLatestResult();
+            List<PhotonTrackedTarget> targets = results.getTargets();
+
+            List<Pose2d> poses = new ArrayList<Pose2d>();
+            for(PhotonTrackedTarget tgt: targets){
+                double d = m_robotToNoteCam.getZ()/Math.tan(tgt.getPitch());
+                double x = d*Math.cos(tgt.getYaw());
+                double y = d*Math.sin(tgt.getYaw());
+                poses.add(new Pose2d(x,y,new Rotation2d(0)));
+            }
+            return poses;
+
         }
-
-        public void updateOdometry(SwerveDrivePoseEstimator odometry, Field2d field) {
-            if (!m_aprilTagCamera.isConnected())
-                return;
-
-            if (m_aprilTagFieldLayout == null)
-                return;
-
-            PhotonPipelineResult targetResult = m_aprilTagCamera.getLatestResult();
-
-            SmartDashboard.putBoolean("vision/hasTargets", targetResult.hasTargets());
-            if (!targetResult.hasTargets()) {
-                // if no target, clean out the numbers
-                SmartDashboard.putNumber("vision/targetID", -1);
-
-                if (PLOT_TAG_SOLUTIONS) {
-                    clearTagSolutions(field);
-                }    
-                return;
-            } 
-            
-            // debugging/display assistance
-            if (PLOT_TAG_SOLUTIONS) {
-                plotVisibleTags(field, targetResult);
-            }    
         
-            // Estimate the robot pose.
-            // If successful, update the odometry using the timestamp of the measurement
-            Optional<EstimatedRobotPose> result = getEstimatedGlobalPose(odometry.getEstimatedPosition());
-            SmartDashboard.putNumber("vision/timestamp", targetResult.getTimestampSeconds());
-            SmartDashboard.putBoolean("vision/foundSolution", result.isPresent());
-            if (result.isPresent()) {
-                EstimatedRobotPose camPose = result.get();
-                SmartDashboard.putString("vision/solutionMethod", camPose.strategy.toString());
-
-                Pose2d estimatedPose = camPose.estimatedPose.toPose2d();
-                plotPose(field, "visionPose", estimatedPose);
-
-                double curImageTimeStamp = targetResult.getTimestampSeconds();
-                odometry.addVisionMeasurement(estimatedPose, curImageTimeStamp);
-
-                if (PLOT_TAG_SOLUTIONS) {
-                    if (camPose.strategy != PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR && camPose.strategy != PoseStrategy.MULTI_TAG_PNP_ON_RIO) {
-                        plotAlternateSolution(field, targetResult);
-                    }
-                    else {
-                        plotPose(field, "visionAltPose", null);
-                    }
-                }
-            }
-            else if (PLOT_TAG_SOLUTIONS) {
-                plotPose(field, "visionPose", null);
-                plotPose(field, "visionAltPose", null);
-            }
-        }
-
+  
         private Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
             try {
                 m_photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
