780b65f5b72c85a00b5390a3a256877e06aa23c5
weepfrog <housmanzach@gmail.com>
2024-01-31 22:27:09 -0500

    made field constants more clear

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -9,10 +9,9 @@ public class FieldConstants {
     S = stage
     M = Midline
      */
-    private final double NOTE_C_X = 7.85;
-    private final double NOTE_S_X = 2.45;
-    private final double NOTECHECK_X = NOTE_C_X - 1;
-
+    private final double NOTE_C_X = 8.28;
+    private final double NOTE_S_X = 2.89;
+    private final double NOTE_CHECK_X = 6.85;
 
     public final Pose2d NOTE_C_1 = new Pose2d(NOTE_C_X, 0.78, Rotation2d.fromDegrees(0));
     public final Pose2d NOTE_C_2 = new Pose2d(NOTE_C_X, 2.44, Rotation2d.fromDegrees(0));
@@ -20,24 +19,38 @@ public class FieldConstants {
     public final Pose2d NOTE_C_4 = new Pose2d(NOTE_C_X, 5.77, Rotation2d.fromDegrees(0));
     public final Pose2d NOTE_C_5 = new Pose2d(NOTE_C_X, 7.45, Rotation2d.fromDegrees(0));
 
-    public final Pose2d NOTE_S_1 = new Pose2d(2.49, 4.26, Rotation2d.fromDegrees(157.45));
-    public final Pose2d NOTE_S_2 = new Pose2d(2.43, 5.55, Rotation2d.fromDegrees(0));
-    public final Pose2d NOTE_S_3 = new Pose2d(2.49, 6.81, Rotation2d.fromDegrees(161.94));
+    public final Pose2d NOTE_S_1 = new Pose2d(NOTE_S_X, 4.26, Rotation2d.fromDegrees(0));
+    public final Pose2d NOTE_S_2 = new Pose2d(NOTE_S_X, 5.52, Rotation2d.fromDegrees(0));
+    public final Pose2d NOTE_S_3 = new Pose2d(NOTE_S_X, 7.00, Rotation2d.fromDegrees(0));
+
+   
+   
+   
+   
+    public final Pose2d ROBOT_NOTE_C_1 = new Pose2d(7.85, 0.78, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_C_2 = new Pose2d(7.85, 2.44, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_C_3 = new Pose2d(7.85, 4.10, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_C_4 = new Pose2d(7.85, 5.77, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_C_5 = new Pose2d(7.85, 7.45, Rotation2d.fromDegrees(0));
+
+    public final Pose2d ROBOT_NOTE_S_1 = new Pose2d(2.49, 4.26, Rotation2d.fromDegrees(157.45));
+    public final Pose2d ROBOT_NOTE_S_2 = new Pose2d(2.43, 5.55, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_S_3 = new Pose2d(2.49, 6.81, Rotation2d.fromDegrees(161.94));
 
     
     //these are all placeholders and subject to change 
-    public final Pose2d SHOOT_M_1 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
-    public final Pose2d SHOOT_M_2 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_SHOOT_M_1 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_SHOOT_M_2 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
 
-    public final Pose2d NOTECHECK_1 = new Pose2d(NOTECHECK_X, 0.78,Rotation2d.fromDegrees(0));
-    public final Pose2d NOTECHECK_2 = new Pose2d(NOTECHECK_X, 2.44,Rotation2d.fromDegrees(0));
-    public final Pose2d NOTECHECK_3 = new Pose2d(NOTECHECK_X, 5.77, Rotation2d.fromDegrees(0));
-    public final Pose2d NOTECHECK_4 = new Pose2d(NOTECHECK_X, 7.45, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_CHECK_1 = new Pose2d(NOTE_CHECK_X, 0.78,Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_CHECK_2 = new Pose2d(NOTE_CHECK_X, 2.44,Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_CHECK_3 = new Pose2d(NOTE_CHECK_X, 5.77, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_NOTE_CHECK_4 = new Pose2d(NOTE_CHECK_X, 7.45, Rotation2d.fromDegrees(0));
 
 
 
-    public final Pose2d START_1 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
-    public final Pose2d START_2 = new Pose2d(1.3, 5.53, Rotation2d.fromDegrees(0));
-    public final Pose2d START_3 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_START_1 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_START_2 = new Pose2d(1.3, 5.53, Rotation2d.fromDegrees(0));
+    public final Pose2d ROBOT_START_3 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
 
 }
