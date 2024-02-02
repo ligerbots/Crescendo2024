2547d3543521748428a0423b8fc10605fa463a9a
SuperYoshi10000 <superyoshi10000@gmail.com>
2024-02-01 20:50:48 -0500

    Add button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 
 import frc.robot.commands.*;
@@ -38,12 +41,25 @@ public class RobotContainer {
         m_controller.b().onTrue(new SetElevatorLength(m_elevator, 10));
         
         m_controller.y().onTrue(new TestShootSpeed(m_shooter,
-            ()->{ return SmartDashboard.getNumber("shooter/test_left_rpm", 0); },
-            ()->{ return SmartDashboard.getNumber("shooter/test_right_rpm", 0); }));
+            () -> SmartDashboard.getNumber("shooter/test_left_rpm", 0),
+            () -> SmartDashboard.getNumber("shooter/test_right_rpm", 0)));
             
         m_controller.x().onTrue(new Shoot(m_shooter,
-            ()->{ return SmartDashboard.getNumber("shooter/test_left_rpm", 0); },
-            ()->{ return SmartDashboard.getNumber("shooter/test_right_rpm", 0); }));
+            () -> SmartDashboard.getNumber("shooter/test_left_rpm", 0),
+            () -> SmartDashboard.getNumber("shooter/test_right_rpm", 0)));
+
+        // SysId stuff
+        m_shooter.setupSysId();
+        
+        JoystickButton farm1 = new JoystickButton(m_farm, 1);
+        JoystickButton farm2 = new JoystickButton(m_farm, 2);
+        JoystickButton farm3 = new JoystickButton(m_farm, 3);
+        JoystickButton farm4 = new JoystickButton(m_farm, 4);
+        
+        farm1.onTrue(m_shooter.sysIdQuasistatic(Direction.kForward));
+        farm2.onTrue(m_shooter.sysIdQuasistatic(Direction.kReverse));
+        farm3.onTrue(m_shooter.sysIdDynamic(Direction.kForward));
+        farm4.onTrue(m_shooter.sysIdDynamic(Direction.kReverse));
     }
 
     public Command getAutonomousCommand() {
