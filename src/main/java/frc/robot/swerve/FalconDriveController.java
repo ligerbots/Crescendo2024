9c185fefb32035dbb407ca750a4a63925ac3f454
weepfrog <housmanzach@gmail.com>
2024-02-08 18:33:07 -0500

    current limit

--- src/main/java/frc/robot/swerve/FalconDriveController.java
+++ src/main/java/frc/robot/swerve/FalconDriveController.java
@@ -8,24 +8,26 @@ import edu.wpi.first.math.util.Units;
 
 public class FalconDriveController implements DriveController {
     private final TalonFX m_motor;
-    private final CurrentLimitsConfigs m_motorCurrentConfigs;
     private static final double WHEEL_DIAMETER = Units.inchesToMeters(4.0);
     // This is the L2 gearing
     private static final double DRIVE_REDUCTION = (14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0);
     private static final double FALCON_DISTANCE_PER_UNIT = Math.PI * WHEEL_DIAMETER * DRIVE_REDUCTION / 2048.0 ;
     
     private static final double CURRENT_LIMIT = 35.0;
+    private static final double CURRENT_LIMIT_TIME = 0.25;
     
     private static final boolean MOTOR_INVERTED = true;
 
     public FalconDriveController(int id) {
         m_motor = new TalonFX(id);
+        CurrentLimitsConfigs motorCurrentConfigs;
         m_motor.setInverted(MOTOR_INVERTED);
-        m_motorCurrentConfigs = new CurrentLimitsConfigs();
-        m_motorCurrentConfigs.withStatorCurrentLimit(CURRENT_LIMIT);
-        m_motorCurrentConfigs.withStatorCurrentLimitEnable(true);
+        motorCurrentConfigs = new CurrentLimitsConfigs();
+        motorCurrentConfigs.withSupplyCurrentThreshold(CURRENT_LIMIT);
+        motorCurrentConfigs.withSupplyTimeThreshold(CURRENT_LIMIT_TIME);
+        motorCurrentConfigs.withStatorCurrentLimitEnable(true);
         // TODO current limit
-        m_motor.getConfigurator().apply(m_motorCurrentConfigs);
+        m_motor.getConfigurator().apply(motorCurrentConfigs);
         m_motor.setNeutralMode(NeutralModeValue.Brake);
     }
 
