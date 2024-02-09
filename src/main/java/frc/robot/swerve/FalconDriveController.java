671c9067da5186e1057c0a7245bdba3ff0cd23c2
Paul Rensing <prensing@ligerbots.org>
2024-02-09 09:02:30 -0500

    Turn on supply current limiting, not stator.

--- src/main/java/frc/robot/swerve/FalconDriveController.java
+++ src/main/java/frc/robot/swerve/FalconDriveController.java
@@ -7,27 +7,28 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.util.Units;
 
 public class FalconDriveController implements DriveController {
-    private final TalonFX m_motor;
     private static final double WHEEL_DIAMETER = Units.inchesToMeters(4.0);
     // This is the L2 gearing
     private static final double DRIVE_REDUCTION = (14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0);
     private static final double FALCON_DISTANCE_PER_UNIT = Math.PI * WHEEL_DIAMETER * DRIVE_REDUCTION / 2048.0 ;
     
-    private static final double CURRENT_LIMIT = 35.0;
-    private static final double CURRENT_LIMIT_TIME = 0.25;
+    private static final double CURRENT_LIMIT = 35.0;  // Amps
+    private static final double CURRENT_LIMIT_TIME = 0.25;  // seconds
     
     private static final boolean MOTOR_INVERTED = true;
 
+    private final TalonFX m_motor;
+
     public FalconDriveController(int id) {
         m_motor = new TalonFX(id);
-        CurrentLimitsConfigs motorCurrentConfigs;
         m_motor.setInverted(MOTOR_INVERTED);
-        motorCurrentConfigs = new CurrentLimitsConfigs();
+    
+        CurrentLimitsConfigs motorCurrentConfigs = new CurrentLimitsConfigs();
         motorCurrentConfigs.withSupplyCurrentThreshold(CURRENT_LIMIT);
         motorCurrentConfigs.withSupplyTimeThreshold(CURRENT_LIMIT_TIME);
-        motorCurrentConfigs.withStatorCurrentLimitEnable(true);
-        // TODO current limit
+        motorCurrentConfigs.withSupplyCurrentLimitEnable(true);
         m_motor.getConfigurator().apply(motorCurrentConfigs);
+    
         m_motor.setNeutralMode(NeutralModeValue.Brake);
     }
 
