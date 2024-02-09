9c0f3846a1c1c7d7f8986dda7954b3f4007262cd
weepfrog <housmanzach@gmail.com>
2024-02-09 17:44:06 -0500

    added field lines

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -12,6 +12,9 @@ public class FieldConstants {
     public static final double FIELD_LENGTH = 16.54;
     public static final double FIELD_WIDTH = 8.21;
 
+    public static final Pose2d BLUE_WHITE_LINE_X_METERS = new Pose2d(1.93294, 0.0, new Rotation2d(0));
+    public static final Pose2d BLUE_WING_LINE_X_METERS = new Pose2d(5.87248, 0, new Rotation2d(0));
+
     /*
      * C = center
      * S = stage
@@ -29,8 +32,8 @@ public class FieldConstants {
 
     public static final Pose2d NOTE_S_1 = new Pose2d(NOTE_S_X, 4.26, Rotation2d.fromDegrees(0));
     public static final Pose2d NOTE_S_2 = new Pose2d(NOTE_S_X, 5.52, Rotation2d.fromDegrees(0));
-    public static final Pose2d NOTE_S_3 = new Pose2d(NOTE_S_X, 7.00, Rotation2d.fromDegrees(0));   
-   
+    public static final Pose2d NOTE_S_3 = new Pose2d(NOTE_S_X, 7.00, Rotation2d.fromDegrees(0));
+
     public static final Pose2d ROBOT_NOTE_C_1 = new Pose2d(7.85, 0.78, Rotation2d.fromDegrees(0));
     public static final Pose2d ROBOT_NOTE_C_2 = new Pose2d(7.85, 2.44, Rotation2d.fromDegrees(0));
     public static final Pose2d ROBOT_NOTE_C_3 = new Pose2d(7.85, 4.10, Rotation2d.fromDegrees(0));
@@ -41,24 +44,24 @@ public class FieldConstants {
     public static final Pose2d ROBOT_NOTE_S_2 = new Pose2d(2.43, 5.55, Rotation2d.fromDegrees(0));
     public static final Pose2d ROBOT_NOTE_S_3 = new Pose2d(2.49, 6.81, Rotation2d.fromDegrees(161.94));
 
-    // these are all placeholders and subject to change 
-    public static final Pose2d ROBOT_SHOOT_M_1 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
-    public static final Pose2d ROBOT_SHOOT_M_2 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    // these are all placeholders and subject to change
+    public static final Pose2d ROBOT_SHOOT_M_1 = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
+    public static final Pose2d ROBOT_SHOOT_M_2 = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
 
-    public static final Pose2d ROBOT_NOTE_CHECK_1 = new Pose2d(NOTE_CHECK_X, 0.78,Rotation2d.fromDegrees(0));
-    public static final Pose2d ROBOT_NOTE_CHECK_2 = new Pose2d(NOTE_CHECK_X, 2.44,Rotation2d.fromDegrees(0));
+    public static final Pose2d ROBOT_NOTE_CHECK_1 = new Pose2d(NOTE_CHECK_X, 0.78, Rotation2d.fromDegrees(0));
+    public static final Pose2d ROBOT_NOTE_CHECK_2 = new Pose2d(NOTE_CHECK_X, 2.44, Rotation2d.fromDegrees(0));
     public static final Pose2d ROBOT_NOTE_CHECK_3 = new Pose2d(NOTE_CHECK_X, 5.77, Rotation2d.fromDegrees(0));
     public static final Pose2d ROBOT_NOTE_CHECK_4 = new Pose2d(NOTE_CHECK_X, 7.45, Rotation2d.fromDegrees(0));
 
-    public static final Pose2d ROBOT_START_1 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    public static final Pose2d ROBOT_START_1 = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
     public static final Pose2d ROBOT_START_2 = new Pose2d(1.3, 5.53, Rotation2d.fromDegrees(0));
-    public static final Pose2d ROBOT_START_3 = new Pose2d(0, 0 ,Rotation2d.fromDegrees(0));
+    public static final Pose2d ROBOT_START_3 = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
 
     public static Pose2d flipPose(Pose2d pose) {
         Optional<Alliance> alliance = DriverStation.getAlliance();
-        
+
         // flip pose when red (have to do this because PathPlanner flips a path only)
-        if (alliance.isPresent() && alliance.get() == Alliance.Red){
+        if (alliance.isPresent() && alliance.get() == Alliance.Red) {
             Rotation2d rot = pose.getRotation();
             // reflect the pose over center line, flip both the X and the rotation
             return new Pose2d(FIELD_LENGTH - pose.getX(), pose.getY(), new Rotation2d(-rot.getCos(), rot.getSin()));
