4a609b3c4c77d587b4c03fe05165384904b6d66d
Paul Rensing <prensing@ligerbots.org>
2024-02-13 09:16:33 -0500

    Small clean ups.

--- src/main/java/frc/robot/commands/RunIntakeWaitNote.java
+++ src/main/java/frc/robot/commands/RunIntakeWaitNote.java
@@ -4,24 +4,20 @@
 
 package frc.robot.commands;
 
-
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
 public class RunIntakeWaitNote extends Command {
 
-
-    /** Creates a new RunIntakeWaitNote. */
     static final double TIME_TO_RUN_INTAKE = 3.0;
     Intake m_intake;
-    Timer m_timer;
-    
-   
-      public RunIntakeWaitNote(Intake intake) {
+    Timer m_timer = new Timer();
+
+    /** Creates a new RunIntakeWaitNote. */
+    public RunIntakeWaitNote(Intake intake) {
         // Use addRequirements() here to declare subsystem dependencies.
         m_intake = intake;
-        m_timer = new Timer();
 
         addRequirements(m_intake);
     }
@@ -32,7 +28,7 @@ public class RunIntakeWaitNote extends Command {
         m_timer.reset();
         m_timer.start();
 
-        m_intake.intake(); 
+        m_intake.intake();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -43,14 +39,14 @@ public class RunIntakeWaitNote extends Command {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-      m_intake.stop(); // check what goes inside the parenthesis
+        m_intake.stop(); 
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
         return m_timer.hasElapsed(TIME_TO_RUN_INTAKE);
-        // another way could be garage sensors.
+        // another way could be a beam break sensor.
         // another way could be how the motors slow down
     }
 }
