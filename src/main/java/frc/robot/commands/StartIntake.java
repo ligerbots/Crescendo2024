a896b187727864ebed0b648d960e20a84c88e00f
Paul Rensing <prensing@ligerbots.org>
2024-02-19 16:56:28 -0500

    Change StartIntake for early testing

--- src/main/java/frc/robot/commands/StartIntake.java
+++ src/main/java/frc/robot/commands/StartIntake.java
@@ -4,26 +4,29 @@
 
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
-import edu.wpi.first.wpilibj.XboxController;
+// import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.ShooterPivot;
 
 public class StartIntake extends SequentialCommandGroup {
   /** Creates a new startIntake. */
-  public StartIntake(Intake intake, Shooter shooter, XboxController xbox, Elevator elevator, DoubleSupplier length, DoubleSupplier angle) /*add shooter pivot*/{
+  public StartIntake(Intake intake, Shooter shooter, Elevator elevator, ShooterPivot pivot) {
     addCommands(
-      new SetElevatorLength(elevator, length),
-      // set angle???
-      new InstantCommand(shooter::turnOnFeeder),
-      new RunIntakeWaitNote(intake),
-      new BackupFeed(shooter),
-      new Rumble(xbox)
+      // turn off shooter completely, just in case
+      new InstantCommand(shooter::turnOffShooter, shooter),
+      // make sure the shooter is in the correct position
+      new SetElevatorLength(elevator, Elevator.STOW_LENGTH)
+          .alongWith(new SetPivotAngle(pivot, ShooterPivot.STOW_ANGLE_RADIANS)),
+      new InstantCommand(shooter::turnOnFeeder, shooter),
+      new InstantCommand(intake::intake, intake)
+      // new RunIntakeWaitNote(intake),
+      // new BackupFeed(shooter),
+      // new Rumble(xbox)
     );
   }
 }
