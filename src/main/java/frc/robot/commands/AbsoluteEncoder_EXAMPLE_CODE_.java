3bee59f3fd750e7c7638a54616bd77f9762deeee
SuperYoshi10000 <superyoshi10000@gmail.com>
2024-02-23 19:59:32 -0500

    Also add relative encoder to compare

--- src/main/java/frc/robot/commands/AbsoluteEncoder_EXAMPLE_CODE_.java
+++ src/main/java/frc/robot/commands/AbsoluteEncoder_EXAMPLE_CODE_.java
@@ -2,15 +2,19 @@ package frc.robot.commands;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 
 public class AbsoluteEncoder_EXAMPLE_CODE_ {
     final CANSparkMax m_motor;
-    final AbsoluteEncoder m_encoder;
+    final AbsoluteEncoder m_absoluteEncoder;
+    final RelativeEncoder m_relativeEncoder;
 
     public AbsoluteEncoder_EXAMPLE_CODE_(CANSparkMax motor) {
         super();
         m_motor = motor;
-        m_encoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
+        // A lot of functions are the same for both types
+        m_absoluteEncoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
+        m_relativeEncoder = motor.getEncoder();
     }
 }
