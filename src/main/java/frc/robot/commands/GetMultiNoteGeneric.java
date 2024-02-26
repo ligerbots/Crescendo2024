d21d86e4b40860809177f529cb29e2848df7b101
jacobh1 <jake.hendrickson@gmail.com>
2024-02-25 19:29:57 -0500

    Add support for wait in Auto, ie. S1-W-S2

--- src/main/java/frc/robot/commands/GetMultiNoteGeneric.java
+++ src/main/java/frc/robot/commands/GetMultiNoteGeneric.java
@@ -9,7 +9,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.FieldConstants;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Intake;
@@ -20,29 +22,38 @@ public class GetMultiNoteGeneric extends SequentialCommandGroup {
 
     private static final Map<String, Translation2d> s_noteNameMap = new HashMap<>() {
         {
-            put("C1", FieldConstants.NOTE_C_1);            
+            put("C1", FieldConstants.NOTE_C_1);
             put("C2", FieldConstants.NOTE_C_2);
             put("C3", FieldConstants.NOTE_C_3);
             put("C4", FieldConstants.NOTE_C_4);
             put("C5", FieldConstants.NOTE_C_5);
-            
+
             put("S1", FieldConstants.BLUE_NOTE_S_1);
             put("S2", FieldConstants.BLUE_NOTE_S_2);
             put("S3", FieldConstants.BLUE_NOTE_S_3);
+
+            put("W", FieldConstants.DUMMY_NOTE_WAIT_FLAG);
         }
     };
 
+    private static final double WAIT_INTERVAL_SECONDS = 1.0;
+
     public GetMultiNoteGeneric(String noteSequence, DriveTrain driveTrain, NoteVision noteVision,
             Shooter shooter, Intake intake) {
-                // Convenience constructor to use an encoded note sequence string i.e. "C1-C2-S3"
-            this(buildNoteList(noteSequence), driveTrain, noteVision, shooter, intake);
-            }
+        // Convenience constructor to use an encoded note sequence string i.e.
+        // "C1-C2-S3"
+        this(buildNoteList(noteSequence), driveTrain, noteVision, shooter, intake);
+    }
 
     public GetMultiNoteGeneric(Translation2d[] noteLocations, DriveTrain driveTrain, NoteVision noteVision,
             Shooter shooter, Intake intake) {
         // add all the fetching+shooting NOTE blocks
         for (Translation2d note : noteLocations) {
-            if (FieldConstants.isCenterNote(note)) {
+            if (FieldConstants.DUMMY_NOTE_WAIT_FLAG == note) {
+                addCommands(new PrintCommand("starting wait " + WAIT_INTERVAL_SECONDS + " seconds"),
+                            new WaitCommand(WAIT_INTERVAL_SECONDS),
+                            new PrintCommand("finished wait " + WAIT_INTERVAL_SECONDS + " seconds"));
+            } else if (FieldConstants.isCenterNote(note)) {
                 addCommands(new GetCenterNoteX(note, driveTrain, noteVision, shooter, intake));
             } else {
                 addCommands(new GetStageNoteX(note, driveTrain, noteVision, shooter, intake));
@@ -51,13 +62,14 @@ public class GetMultiNoteGeneric extends SequentialCommandGroup {
     }
 
     public static Translation2d[] buildNoteList(String noteSequence) {
-        
+
         String[] separateNoteNames = noteSequence.split("\\s*-\\s*");
         Translation2d[] noteCoordList = new Translation2d[separateNoteNames.length];
-        for(int i=0; i<separateNoteNames.length; i++) {
+
+        for (int i = 0; i < separateNoteNames.length; i++) {
             Translation2d foundNote = s_noteNameMap.get(separateNoteNames[i].toUpperCase());
-            if(foundNote == null) {
-                throw new IllegalArgumentException("note name not found: "+ separateNoteNames[i]);
+            if (foundNote == null) {
+                throw new IllegalArgumentException("note name not found: " + separateNoteNames[i]);
             }
             noteCoordList[i] = foundNote;
         }
