459ad24a571869819493be348376a5b52855314a
Paul Rensing <prensing@ligerbots.org>
2024-03-01 12:22:20 -0500

    it is always a unit bug

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -46,7 +46,7 @@ public class Robot extends TimedRobot {
         
         boolean isRedAlliance = FieldConstants.isRedAlliance();
         if (isRedAlliance != m_prevIsRedAlliance || m_robotContainer.autoHasChanged()) {
-                        m_robotContainer.getDriveTrain().setPose(m_robotContainer.getInitialPose());
+            m_robotContainer.getDriveTrain().setPose(m_robotContainer.getInitialPose());
             m_prevIsRedAlliance = isRedAlliance;
         }
     }
