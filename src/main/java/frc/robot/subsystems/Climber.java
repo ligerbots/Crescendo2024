4243c25e0a767a652bdf486ad508c213162c38e9
Paul Rensing <prensing@gmail.com>
2024-03-09 10:56:24 -0500

    bug: does not engage if level

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -35,12 +35,15 @@ public class Climber extends SubsystemBase {
     // private static final double NOMINAL_WINCH_DIAMETER = Units.inchesToMeters(0.75);
     // private static final double NOMINAL_INCHES_PER_ROTATION = Math.PI * NOMINAL_WINCH_DIAMETER * WINCH_GEAR_RATIO;
     private static final double HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION = 100.0;
-    private static final double CLIMB_ROTATIONS_FINAL = 190.0;
+    private static final double CLIMB_ROTATIONS_FINAL = 225.0;
+
 
     // Protection values
     private static final double MAX_WINCH_ROTATIONS_ALLOWED = 235.0;
     // TODO set a reasonable value
     private static final double MAX_WINCH_CURRENT = 100.0;
+
+    private static final double MAX_ROTATION_RETRACT = 150.0;
 
     // Winch motor speed values
     private static final double IDLE_MOTOR_SPEED = -0.01;
@@ -172,13 +175,14 @@ public class Climber extends SubsystemBase {
             // TODO: what about if they both engage at the same time and stay ~level?
             //  Maybe also look at current?
 
-            if (m_rollAngle > ROLL_ANGLE_TOLERANCE) {
+            if (m_rollAngle > ROLL_ANGLE_TOLERANCE || m_leftPosition > MAX_ROTATION_RETRACT) {
                 // The left side is high, so the left hook is engaged.
                 // Stop the left motor. The ratchet wrench will hold it.
                 m_leftWinch.set(0.0);
                 m_leftHookEngaged = true;
                 // m_leftEngagedPosition = m_leftPosition;
-            } else if (m_rollAngle < -ROLL_ANGLE_TOLERANCE) {
+            } 
+            if (m_rollAngle < -ROLL_ANGLE_TOLERANCE || m_rightPosition > MAX_ROTATION_RETRACT) {
                 // The right side is high, so the right hook is engaged.
                 // Stop the right motor. The ratchet wrench will hold it.
                 m_rightWinch.set(0.0);
