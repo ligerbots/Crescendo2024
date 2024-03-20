faca5fb581a2749905990ca9c8a4a5efdc15d8e4
Paul Rensing <prensing@ligerbots.org>
2024-03-20 09:48:52 -0400

    clean up comments. add trapezoidal slow down at end of extend

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -45,11 +45,13 @@ public class Climber extends SubsystemBase {
 
     // Winch motor speed values
     private static final double IDLE_MOTOR_SPEED = -0.01;
-    private static final double WINCH_EXTEND_SPEED = 0.5;
+    private static final double WINCH_EXTEND_MAX_SPEED = 0.7;
+    private static final double WINCH_EXTEND_MIN_SPEED = 0.2;
     private static final double WINCH_RETRACT_SPEED = 0.5;
     public static final double WINCH_MANUAL_SPEED = 0.3;
     private static final double WINCH_CLIMB_SPEED = 0.5;
-    private static final double WINCH_CLIMB_ADJUST_SPEED = 0.5;
+    // speed adjustment amount if Roll is off by ROLL_ANGLE_TOLERANCE
+    private static final double WINCH_CLIMB_ADJUST_SPEED = 0.1;
     private static final double ROLL_ANGLE_TOLERANCE = Units.degreesToRadians(2.0);
     private static final double ROLL_ANGLE_EMERGENCY_STOP = Units.degreesToRadians(15.0);
 
@@ -111,6 +113,8 @@ public class Climber extends SubsystemBase {
 
         SmartDashboard.putNumber("climber/leftPosition", leftPosition);
         SmartDashboard.putNumber("climber/rightPosition", rightPosition);
+        SmartDashboard.putNumber("climber/leftEngagePosition", m_leftEngagedPosition);
+        SmartDashboard.putNumber("climber/rightEngagePosition", m_rightEngagedPosition);
 
         SmartDashboard.putNumber("climber/leftCurrent", leftCurrent);
         SmartDashboard.putNumber("climber/rightCurrent", rightCurrent);
@@ -118,13 +122,6 @@ public class Climber extends SubsystemBase {
         SmartDashboard.putNumber("climber/roll", Math.toDegrees(rollAngle));
         SmartDashboard.putNumber("climber/pitch", m_driveTrain.getPitch().getDegrees());
         SmartDashboard.putString("climber/state", m_climberState.toString());
-
-        // Protection: stop if position is at max or current is too high
-        // Test no matter what State we are in. 
-        if (leftPosition >= MAX_WINCH_ROTATIONS_ALLOWED || leftCurrent > MAX_WINCH_CURRENT)
-            m_leftWinch.set(0);
-        if (rightPosition >= MAX_WINCH_ROTATIONS_ALLOWED || leftCurrent > MAX_WINCH_CURRENT)
-            m_rightWinch.set(0);
 
         // Always check if we're too far off balance, but only while climbing.
         // If the robot tips while driving, don't worry about it.
@@ -141,17 +138,30 @@ public class Climber extends SubsystemBase {
             // Note that the EXTENDING_HOOKS state is entered via a command, so here we just stay IDLE
         }
         else if (m_climberState == ClimberState.EXTENDING_HOOKS) {
+            // use a "trapezoidal" profile to slow down near the end
+
             // If the left hook is all the way out...
-            if (leftPosition > HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION) {
+            if (leftPosition >= HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION) {
                 // Stop left winch
                 m_leftWinch.set(0.0);
                 m_leftHookReadyToEngage = true;
             }
+            else {
+                // slow down over the last 20 rotations
+                double speed = (HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION - leftPosition) / 20.0 * WINCH_EXTEND_MAX_SPEED;
+                m_leftWinch.set(MathUtil.clamp(speed, WINCH_EXTEND_MIN_SPEED, WINCH_EXTEND_MAX_SPEED));
+            }
+
             // If the right hook is all the way out...
-            if (rightPosition > HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION) {
+            if (rightPosition >= HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION) {
                 // Stop right winch
                 m_rightWinch.set(0.0);
                 m_rightHookReadyToEngage = true;
+            }
+            else {
+                // slow down over the last 20 rotations
+                double speed = (HOOK_DEPLOYED_ROTATIONS_ABOVE_INITIAL_POSITION - rightPosition) / 20.0 * WINCH_EXTEND_MAX_SPEED;
+                m_leftWinch.set(MathUtil.clamp(speed, WINCH_EXTEND_MIN_SPEED, WINCH_EXTEND_MAX_SPEED));
             }
 
             // If both hooks are all the way up, wait to retract
@@ -167,7 +177,7 @@ public class Climber extends SubsystemBase {
             // Here we retract both hooks. When one hook grabs the chain before the other, the robot will start to roll
             // Roll angle is positive if the left side is higher than the right side.
             // Once one side goes high, we need to stop that winch until the other hook "catches up"
-            // Note that when we enetered this state, both winches started retracting. We only need to stop them one at a time until
+            // Note that when we entered this state, both winches started retracting. We only need to stop them one at a time until
             // the robot is level again and then we will go to the CLIMBING state
 
             // Also check for position going well past the Raise position. This fixes the case
@@ -180,6 +190,7 @@ public class Climber extends SubsystemBase {
                 m_leftHookEngaged = true;
                 m_leftEngagedPosition = leftPosition;
             } 
+
             if (rollAngle < -ROLL_ANGLE_TOLERANCE || rightPosition > MAX_ROTATION_RETRACT) {
                 // The right side is high, so the right hook is engaged.
                 // Stop the right motor. The ratchet wrench will hold it.
@@ -198,29 +209,43 @@ public class Climber extends SubsystemBase {
         }
         else if (m_climberState == ClimberState.CLIMBING) {
             // If we climbed far enough, stop the winches and let the ratchets hold the robot.
-            //TODO: Check if roll & pitch in drivetrain are swaped correctly, may need to flip sign.
-            final double ADJUST_SPEED = Math.signum(rollAngle) *
-                        Math.min(Math.abs(rollAngle)/ROLL_ANGLE_TOLERANCE, 1.0) * WINCH_CLIMB_ADJUST_SPEED;
+            // TODO: Check if roll & pitch in drivetrain are swapped correctly, may need to flip sign.
+            // roll > 0 means left side is high, and adjustSpeed is also > 0
+            final double adjustSpeed = WINCH_CLIMB_ADJUST_SPEED * (rollAngle / ROLL_ANGLE_TOLERANCE);
 
-            //Left side.
-            if (leftPosition - m_leftEngagedPosition >= CLIMB_ROTATIONS_AFTER_ENGAGE) {
+            // Left side
+            if (leftPosition - m_leftEngagedPosition >= CLIMB_ROTATIONS_AFTER_ENGAGE || leftPosition > MAX_WINCH_ROTATIONS_ALLOWED) {
                 m_leftWinch.set(0.0);
                 m_leftHookComplete = true;
-            } else if (Math.abs(rollAngle) > ROLL_ANGLE_TOLERANCE) { //If not done, adjust speed according to tilt
-                    m_leftWinch.set(MathUtil.clamp(WINCH_CLIMB_SPEED + ADJUST_SPEED,-1,1)); //TODO: Check if sign need flipping
+            } else { // if (Math.abs(rollAngle) > ROLL_ANGLE_TOLERANCE) { 
+                // If not done, adjust speed according to roll
+                // adjustSpeed > 0 means left is high, so slow left down
+                m_leftWinch.set(MathUtil.clamp(WINCH_CLIMB_SPEED - adjustSpeed, 0, 1));
             }
-            //Right side.
-            if (rightPosition - m_rightEngagedPosition >= CLIMB_ROTATIONS_AFTER_ENGAGE) {
+
+            // Right side
+            if (rightPosition - m_rightEngagedPosition >= CLIMB_ROTATIONS_AFTER_ENGAGE || rightPosition > MAX_WINCH_ROTATIONS_ALLOWED) {
                 m_rightWinch.set(0.0);
                 m_rightHookComplete = true;
-            } else if (Math.abs(rollAngle) > ROLL_ANGLE_TOLERANCE) { //If not done, adjust speed according to tilt
-                    m_rightWinch.set(MathUtil.clamp(WINCH_CLIMB_SPEED - ADJUST_SPEED,-1,1)); //TODO: Check if sign need fliping
+            } else { //if (Math.abs(rollAngle) > ROLL_ANGLE_TOLERANCE) { 
+                // If not done, adjust speed according to roll
+                // adjustSpeed > 0 means right is low, so speed right up
+                m_rightWinch.set(MathUtil.clamp(WINCH_CLIMB_SPEED + adjustSpeed, 0, 1));
             }
+
             // If both hooks are in enough, just hold where we are.
             if (m_leftHookComplete && m_rightHookComplete) {
                 m_climberState = ClimberState.HOLDING;
             }
         }
+
+        // Protection: stop if position is at max or current is too high
+        // Test no matter what State we are in. 
+        // Do this at the end to override any settings
+        if (leftPosition >= MAX_WINCH_ROTATIONS_ALLOWED || leftCurrent > MAX_WINCH_CURRENT)
+            m_leftWinch.set(0);
+        if (rightPosition >= MAX_WINCH_ROTATIONS_ALLOWED || leftCurrent > MAX_WINCH_CURRENT)
+            m_rightWinch.set(0);
     }
 
     private double limitWinch(RelativeEncoder encoder, double speed) {
@@ -253,7 +278,7 @@ public class Climber extends SubsystemBase {
 
     public void extendHooks() {
         m_climberState = ClimberState.EXTENDING_HOOKS;
-        run(WINCH_EXTEND_SPEED, WINCH_EXTEND_SPEED);
+        run(WINCH_EXTEND_MAX_SPEED, WINCH_EXTEND_MAX_SPEED);
     }
 
     public void retractHooks() {
