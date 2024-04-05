9c08dc27cc0afa55257e4f91c27a3736e8eb71ee
Paul Rensing <prensing@gmail.com>
2024-04-05 08:31:09 -0400

    slow pass shot even more

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -110,9 +110,9 @@ public class Shooter extends SubsystemBase {
 
             // Pass shots from Center zone
             // close to Stage
-            Map.entry(6.0, new ShooterValues(2000.0, 2000.0, Math.toRadians(50))),
+            Map.entry(6.0, new ShooterValues(1500.0, 1500.0, Math.toRadians(50))),
             // more in center area
-            Map.entry(FieldConstants.FIELD_LENGTH/2, new ShooterValues(2000.0, 2000.0, Math.toRadians(40)))
+            Map.entry(FieldConstants.FIELD_LENGTH/2, new ShooterValues(1500.0, 1500.0, Math.toRadians(40)))
             ));
 
     // Shooter class constructor, initialize arrays for motors controllers,
